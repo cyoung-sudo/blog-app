18bb1959379ea166cd07d76870a3802745c0249b
Christopher Young <christopheryoung@MacBook123.local>
2021-10-07 15:06:49 -0700

    heroku setup

--- server.js
+++ server.js
@@ -17,20 +17,6 @@ mongoose.connect(process.env.MONGO_URI)
     console.log(err)
   })
 
-// const connectDB = async () => {
-//   try {
-//     // MongoDB connection string
-//     const con = await mongoose.connect(process.env.MONGO_URI)
-
-//     console.log(`MongoDB connected: ${con.connection.host}`)
-//   } catch(err) {
-//     console.log(err)
-//     process.exit(1)
-//   }
-// }  
-
-// connectDB()
-
 // Defined port value in config.env
 const PORT = process.env.PORT||8080
 
